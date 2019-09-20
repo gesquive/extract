5833f243efd0299260086dd9f1e38403cb7b2f18
Gus Esquivel <gesquive@gmail.com>
2019-09-20 18:30:55 -0500

    cleanup some functions and logic

--- extract.sh
+++ extract.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Utility script to extract archives
 # https://github.com/gesquive/extract
 
-VERSION="v1.1.0"
+VERSION="v1.1.1"
 
 function usage() {
     echo "Extracts bz2, exe, gz, lzma, rar, tar, tbz2, tgz, xz, z and zip archives"
@@ -28,19 +28,27 @@ elif [ "$1" == "-v" ]||[ "$1" == "--version" ]; then
     exit 1
 fi
 
-function check {
-    if [ ! -x "$(command -v $1)" ]; then
+function check() {
+    if ! type "$1" &> /dev/null; then
         return 1;
     fi
     return 0
 }
 
-function print_missing {
-    echo "Need any of following executable(s) to extract: $1" >&2
+function print() {
+    printf "$*\n"
+}
+
+function print_err() {
+    printf "$*\n" >&2
+}
+
+function print_missing() {
+    print_err "Need any of following executable(s) to extract: $1"
     exit 1
 }
 
-function exe() {
+function echo_run() {
     echo ">>" "$@"
     "$@"
 }
@@ -52,9 +60,9 @@ function extract_tbz2() {
         print_missing "tar, lbunzip2"
     fi
     if check "lbunzip2"; then
-        exe tar --use-compress-program=lbunzip2 -xvf "$1"
+        echo_run tar --use-compress-program=lbunzip2 -xvf "$1"
     else
-        exe tar xvjf "$1"
+        echo_run tar xvjf "$1"
     fi
 }
 
@@ -63,9 +71,9 @@ function extract_tgz() {
         print_missing "tar, unpigz"
     fi
     if check "unpigz"; then
-        exe tar --use-compress-program=unpigz -xvf "$1"
+        echo_run tar --use-compress-program=unpigz -xvf "$1"
     else
-        exe tar xvzf "$1"
+        echo_run tar xvzf "$1"
     fi
 }
 
@@ -74,17 +82,17 @@ function extract_txz() {
         print_missing "tar, pixz"
     fi
     if check "pixz"; then
-        exe tar --use-compress-program=pixz -xvf "$1"
+        echo_run tar --use-compress-program=pixz -xvf "$1"
     else
-        exe tar xvJf "$1"
+        echo_run tar xvJf "$1"
     fi
 }
 
 function extract_bz2() {
     if check "lbunzip2"; then
-        exe lbunzip2 -fk "$1"
+        echo_run lbunzip2 -fk "$1"
     elif check "bunzip2"; then
-        exe bunzip2 -fk "$1"
+        echo_run bunzip2 -fk "$1"
     else
         print_missing "lbunzip2, bunzip2"
     fi
@@ -92,7 +100,7 @@ function extract_bz2() {
 
 function extract_exe() {
     if check "cabextract"; then
-        exe cabextract "$1"
+        echo_run cabextract "$1"
     else
         print_missing "cabextract"
     fi
@@ -100,25 +108,17 @@ function extract_exe() {
 
 function extract_gz() {
     if check "unpigz"; then
-        exe unpigz -fk "$1"
+        echo_run unpigz -fk "$1"
     elif check "gunzip"; then
-        exe gunzip -fk "$1"
+        echo_run gunzip -fk "$1"
     else
         print_missing "unpigz, gunzip"
     fi
 }
 
-function extract_lzma() {
-    if check "unlzma"; then
-        exe unlzma -fk "$1"
-    else
-        print_missing "unlzma"
-    fi
-}
-
 function extract_rar() {
     if check "unrar"; then
-        exe unrar x -ad "$1"
+        echo_run unrar x -ad "$1"
     else
         print_missing "unrar"
     fi
@@ -126,7 +126,7 @@ function extract_rar() {
 
 function extract_tar() {
     if check "tar"; then
-        exe tar xvf "$1"
+        echo_run tar xvf "$1"
     else
         print_missing "tar"
     fi
@@ -134,17 +134,17 @@ function extract_tar() {
 
 function extract_xz() {
     if check "pixz"; then
-        exe pixz -d "$1"
-    elif check "unxz"; then
-        exe unxz -fk "$1"
+        echo_run pixz -d "$1"
+    elif check "xz"; then
+        echo_run xz -dfk "$1"
     else
-        print_missing "pixz, unxz"
+        print_missing "pixz, xz"
     fi
 }
 
 function extract_z() {
     if check "uncompress"; then
-        exe uncompress -f "$1"
+        echo_run uncompress -f "$1"
     else
         print_missing "uncompress"
     fi
@@ -152,7 +152,7 @@ function extract_z() {
 
 function extract_zip() {
     if check "unzip"; then
-        exe unzip "$1"
+        echo_run unzip "$1"
     else
         print_missing "unzip"
     fi
@@ -160,7 +160,7 @@ function extract_zip() {
 
 function extract_7z() {
     if check "7z"; then
-        exe 7z x "$1"
+        echo_run 7z x "$1"
     else
         print_missing "7z"
     fi
@@ -169,35 +169,34 @@ function extract_7z() {
 ###############################################################################
 
 for x in "$@"; do
-    if [ -f "$x" ] ; then
-        NAME="$(echo "$1" | tr '[:upper:]' '[:lower:]')"
-        case "$NAME" in
-            *.tar.bz)   extract_tbz2 "$x" ;;
-            *.tar.bz2)  extract_tbz2 "$x" ;;
-            *.tar.gz)   extract_tgz "$x" ;;
-            *.tar.xz)   extract_txz "$x" ;;
-            *.bz2)      extract_bz2 "$x" ;;
-            *.exe)      extract_exe "$x" ;;
-            *.gz)       extract_gz "$x" ;;
-            *.lzma)     extract_lzma "$x" ;;
-            *.pxz)      extract_xz "$x" ;;
-            *.rar)      extract_rar "$x" ;;
-            *.tar)      extract_tar "$x" ;;
-            *.tbz)      extract_tbz2 "$x" ;;
-            *.tbz2)     extract_tbz2 "$x" ;;
-            *.tz2)      extract_tbz2 "$x" ;;
-            *.tgz)      extract_tgz "$x" ;;
-            *.txz)      extract_txz "$x" ;;
-            *.xz)       extract_xz "$x" ;;
-            *.z)        extract_z "$x" ;;
-            *.zip)      extract_zip "$x" ;;
-            *.7z)       extract_7z "$x" ;;
-            *) echo "extract: '$x' - unknown archive method" ;;
-        esac
-    else
+    if [ ! -f "$x" ] ; then
         echo "'$x' - file does not exist"
 		exit 1;
     fi
+    NAME="$(echo "$1" | tr '[:upper:]' '[:lower:]')"
+    case "$NAME" in
+        *.tar.bz)   extract_tbz2 "$x" ;;
+        *.tar.bz2)  extract_tbz2 "$x" ;;
+        *.tar.gz)   extract_tgz "$x" ;;
+        *.tar.xz)   extract_txz "$x" ;;
+        *.bz2)      extract_bz2 "$x" ;;
+        *.exe)      extract_exe "$x" ;;
+        *.gz)       extract_gz "$x" ;;
+        *.lzma)     extract_xz "$x" ;;
+        *.pxz)      extract_xz "$x" ;;
+        *.rar)      extract_rar "$x" ;;
+        *.tar)      extract_tar "$x" ;;
+        *.tbz)      extract_tbz2 "$x" ;;
+        *.tbz2)     extract_tbz2 "$x" ;;
+        *.tz2)      extract_tbz2 "$x" ;;
+        *.tgz)      extract_tgz "$x" ;;
+        *.txz)      extract_txz "$x" ;;
+        *.xz)       extract_xz "$x" ;;
+        *.z)        extract_z "$x" ;;
+        *.zip)      extract_zip "$x" ;;
+        *.7z)       extract_7z "$x" ;;
+        *) echo "extract: '$x' - unknown archive" ;;
+    esac
 done
 
 exit 0;
